dda20d40a96a86c025dec5b74afa958d0b76d329
Ntokozo <ntokozomweli001@gmail.com>
2023-07-03 06:53:08 +0200

    changed routes again

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -14,7 +14,7 @@ export class ProjectsComponent implements OnInit {
   projects: Project[] = [
     {
       title: 'South Side',
-      image: "/assets/Projects/south side image.jpg",
+      image: "assets/Projects/south side image.jpg",
       technologies: 'Java, Android, Android Studio, Firebase, Google Maps API',
       url: 'https://github.com/ntokom89/OPSC_South_Side_APPLICATION',
       description: 'The android landmark naviagtion application that allows to mark landmarks as favourites, get directions to the landmark and customise user profile.',
@@ -22,7 +22,7 @@ export class ProjectsComponent implements OnInit {
     },
     {
       title: 'Travel App',
-      image: '/assets/Projects/Travel app image.jpg',
+      image: 'assets/Projects/Travel app image.jpg',
       technologies: 'Java, Android, Android Studio, Firebase',
       url: 'https://github.com/ntokom89/Travel__App',
       description: 'This is the travel android app in which the user can take a photo of the place that he/she is adding or use the gallery. The goal can be set by the user of amount of places that the user wants to visit.',
@@ -30,7 +30,7 @@ export class ProjectsComponent implements OnInit {
     },
     {
       title: 'Library Assistant App',
-      image: '/assets/Projects/shelf image FINAL (1).jpg',
+      image: 'assets/Projects/shelf image FINAL (1).jpg',
       technologies: 'C#, ASP.NET, WPF',
       url: 'https://github.com/ntokom89/PROG7321_Task_1',
       description: 'The WPF application allows the user to identify call numbers of the books, sort the books or identify areas in which the call numbers belong to',
@@ -38,7 +38,7 @@ export class ProjectsComponent implements OnInit {
     },
     {
       title: 'Government Blog',
-      image: '/assets/Projects/Blog Image.jpg',
+      image: 'assets/Projects/Blog Image.jpg',
       technologies: 'Angular, NodeJs, Typescript, Javascript, MongoDB, APIs',
       url: 'https://github.com/ntokom89/Government-Blog',
       description: 'A very simple government post blog.',
